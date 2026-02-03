584e4e123d496bac2fafb717b0b711dc7db3cad6
Benoit Glorieux <255315713+benoitglorieuxorange@users.noreply.github.com>
2026-02-03 14:29:06 +0100

    Update interface ISymptomWriter

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,20 +1,18 @@
 package com.hemebiotech.analytics;
 
-import java.util.List;
+/**
+ * Writes the symptoms to the appropriate storage system.
+ * This method is responsible for persisting symptom data.
+ * The concrete implementation will determine the destination (file, database, etc.)
+ * and the writing format.
+ *
+ */
 
 public interface ISymptomWriter {
 
-    /**
 
-     */
-    public interface ISymptomReader {
-        /**
-         *
-         *
-         */
-        public void WriteSymptoms();
+      public void WriteSymptoms();
 
 
-    }
 
 }
